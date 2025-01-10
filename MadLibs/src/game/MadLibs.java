4d15d944c435e8b1a2d2aceaec6c2eda1150b84a
nortoha <norto@192.168.1.72>
2025-01-10 13:33:31 -0600

    adding more

--- MadLibs/src/game/MadLibs.java
+++ MadLibs/src/game/MadLibs.java
@@ -8,6 +8,37 @@ public class MadLibs {
 
 		Scanner scan = new Scanner(System.in);
 		
+		String playAgain = "";
+		
+		do {
+			
+			// play the game
+			System.out.println("Welcome to MadLibs!!  Let's play.");
+			
+			String color = "";
+			System.out.println("Enter a color: ");
+			color = scan.next();
+			
+			String pastTenseVerb = "";
+			System.out.println("Enter a past tense verb: ");
+			pastTenseVerb = scan.next();
+			
+			String adjective = "";
+			System.out.println("Enter an adjective: ");
+			adjective = scan.next();
+			
+			// ask for more....
+			
+			
+			System.out.print("\nThe " + color + " dragon " + pastTenseVerb + " at the ");
+			System.out.println(adjective + " knight.");
+			
+			
+		} while(playAgain.equalsIgnoreCase("y"));
+		
+		System.out.println("Thank you for playing!! Goodbye!");
+		scan.close();
+		
 	}
 
 }
